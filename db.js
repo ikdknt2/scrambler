2199ba5e6346c91088bae66da0b60fe9f777b186
ikdknt2 <ikdknt2@gmail.com>
2026-03-05 16:16:41 +0900

    add delete button for times

--- db.js
+++ db.js
@@ -37,10 +37,33 @@ function loadTimes(){
 
         let out="";
 
-        for(let i=0;i<data.length;i++){
-            out += data[i].time + "<br>";
+        for(let i=data.length-1;i>=0;i--){
+
+            out += `
+            <div>
+            ${data[i].time}
+            <button onclick="deleteTime(${data[i].id})">❌</button>
+            </div>
+            `;
         }
 
         document.getElementById("timeList").innerHTML = out;
     };
 }
+
+function deleteTime(id){
+
+    if(!confirm("このタイムを削除しますか？")){
+        return;
+    }
+
+    const tx = db.transaction("times","readwrite");
+    const store = tx.objectStore("times");
+
+    store.delete(id);
+
+    tx.oncomplete = loadTimes;
+}
+
+window.saveTime = saveTime;
+window.deleteTime = deleteTime;
